b4b76d7adb3659863def266ff790b1dea35874fd
Laksh <yadavlakshya86@gmail.com>
2023-12-07 09:53:23 +0530

    Importing-making dummy json data done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import React, {useState,useEffect} from "react";
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css'
+// Step-1- Making-Importing the dummy json data
+          // the deault value of id-2 has set "true" , rest all are false
+ const seatsData = [
+  {id:1,seatNumber:"A1", booked:false},
+  {id:2,seatNumber:"A1", booked:true},
+  {id:3,seatNumber:"A1", booked:false},
+  {id:4,seatNumber:"A1", booked:false},
+  {id:5,seatNumber:"A1", booked:false},
+  {id:6,seatNumber:"A1", booked:false},
+  {id:7,seatNumber:"A1", booked:false},
+  {id:8,seatNumber:"A1", booked:false},
+  {id:9,seatNumber:"A1", booked:false},
+  {id:10,seatNumber:"A1", booked:false},
+  {id:11,seatNumber:"A1", booked:false},
+  {id:12,seatNumber:"A1", booked:false},
+  // You can modify the data as per your requirement
+ ]
 
 function App() {
   
